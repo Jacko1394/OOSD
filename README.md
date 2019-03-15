c9d8be05a24b9c2b783999cf5224a9e274e6a6a3
Jack Della <jacko1394@me.com>
2019-03-15 12:16:31 +1100

    Readme.md BOOM SUM

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # OOSD
-OOSD Assignment 1
+## Object Oriented Software Design
+### Boom Sum - Assignment 1
+
+[Game Rules](https://docs.google.com/document/d/1_wMhFzSuaV6x1I3Z5ugPdOIbmNKR9TzG59vx2J-8ob8/edit?usp=sharing)
+
+---
+
+#### Model
+* Player
+* BasePiece
+  * ApplePiece
+  * MicrosoftPiece
+#### View
+* MainScreen
+* Board?
+#### Controller
+* Application
+* Game
+
+![alt text](https://koenig-media.raywenderlich.com/uploads/2016/06/MVC-feature.png "MVC")
+
+<!-- ![alt text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/MVC-Process.svg/1024px-MVC-Process.svg.png "MVC2") -->
--- src/game/Main.java
+++ src/game/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        primaryStage.setTitle("Boom Sum");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+
+        var list = root.getChildrenUnmodifiable();
+
     }
 
 
