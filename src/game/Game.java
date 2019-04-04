4f703d225c5a7abe1e3808385e000aba6843a008
patrickdowling41 <patrick.dowling41@gmail.com>
2019-04-04 23:55:47 +1100

    Game class structured

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,10 +1,47 @@
 package game;
 
+import game.board.*;
+import java.util.Random;
+
 public class Game {
 
     private int numberOfTeams;
-    private String noOfNeams;
+    private String currentTeam;
+    private String listOfTeams[] = {"Microsoft", "Apple"};
 
-    
+    public void startGame()
+    {
+        // randomly select a team to go first
+        Random rand = new Random(); 
+        currentTeam = listOfTeams[rand.nextInt(numberOfTeams - 1)];
+
+        Board board = new Board();
+        board.initialiseCells();
+
+    }
+
+    public int getNumberOfTeams() {
+        return numberOfTeams;
+    }
+
+    public void setNumberOfTeams(int numberOfTeams) {
+        this.numberOfTeams = numberOfTeams;
+    }
+
+    public String getCurrentTeam() {
+        return currentTeam;
+    }
+
+    public void setCurrentTeam(String currentTeam) {
+        this.currentTeam = currentTeam;
+    }
+
+    public String[] getListOfTeams() {
+        return listOfTeams;
+    }
+
+    public void setListOfTeams(String[] listOfTeams) {
+        this.listOfTeams = listOfTeams;
+    }
 
 }
