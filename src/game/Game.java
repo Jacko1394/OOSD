0015270aeae6e5737dcb2461d3ebbf6342d5fdf7
patrickdowling41 <patrick.dowling41@gmail.com>
2019-04-06 17:13:20 +1100

    Added next player function

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,46 +1,70 @@
 package game;
 
-import game.board.*;
+import game.board.Board;
 import java.util.Random;
 
 public class Game {
 
     private int numberOfTeams;
     private String currentTeam;
+    // teams can be fed in using stdin at a later stage for more than 2 players
     private String listOfTeams[] = {"Microsoft", "Apple"};
 
     public void startGame()
     {
+        numberOfTeams = listOfTeams.length;
+
         // randomly select a team to go first
         Random rand = new Random(); 
         currentTeam = listOfTeams[rand.nextInt(numberOfTeams - 1)];
 
         Board board = new Board();
         board.initialiseCells();
-
     }
 
-    public int getNumberOfTeams() {
+    public String nextPlayer()
+    {
+        // assume next player is the first player in array, as we compare all but last player
+        String nextPlayer = listOfTeams[0];
+
+        for (int i = 0; i < listOfTeams.length - 1; i++)
+        {
+            if (listOfTeams[i].equals(currentTeam))
+            {
+                nextPlayer = listOfTeams[i + 1];
+            }
+        }
+
+        return nextPlayer;
+    }
+
+    public int getNumberOfTeams() 
+    {
         return numberOfTeams;
     }
 
-    public void setNumberOfTeams(int numberOfTeams) {
+    public void setNumberOfTeams(int numberOfTeams) 
+    {
         this.numberOfTeams = numberOfTeams;
     }
 
-    public String getCurrentTeam() {
+    public String getCurrentTeam() 
+    {
         return currentTeam;
     }
 
-    public void setCurrentTeam(String currentTeam) {
+    public void setCurrentTeam(String currentTeam) 
+    {
         this.currentTeam = currentTeam;
     }
 
-    public String[] getListOfTeams() {
+    public String[] getListOfTeams() 
+    {
         return listOfTeams;
     }
 
-    public void setListOfTeams(String[] listOfTeams) {
+    public void setListOfTeams(String[] listOfTeams) 
+    {
         this.listOfTeams = listOfTeams;
     }
 
