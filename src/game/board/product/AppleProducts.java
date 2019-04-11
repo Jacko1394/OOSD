b20555607998f16621e797dc32d2d32e4a00d8c0
patrickdowling41 <patrick.dowling41@gmail.com>
2019-04-11 17:08:24 +1000

    changes

--- src/game/board/product/AppleProducts.java
+++ src/game/board/product/AppleProducts.java
@@ -4,7 +4,8 @@ public abstract class AppleProducts extends Product {
 
 
     private static final String APPLE = "Apple";
-
+    
+    
 
     public AppleProducts(int[] dice, int positionX, int positionY) {
         super(dice, APPLE, positionX, positionY);
