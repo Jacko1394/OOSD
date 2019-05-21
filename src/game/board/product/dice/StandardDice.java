87a95c7e3fe18861b76ad4e6962e7a5dfd7852b5
patrickdowling41 <s3605032@student.rmit.edu.au>
2019-05-21 20:17:47 +1000

    Patched bug with dice v3

--- src/game/board/product/dice/StandardDice.java
+++ src/game/board/product/dice/StandardDice.java
@@ -2,10 +2,6 @@ package game.board.product.dice;
 
 public class StandardDice extends DiceDecorator {
 
-    public StandardDice(int[] config) {
-        super(config);
-    }
-
     @Override
     public int[] decorate()
     {
